c8b146bac1746d4dbffb049697faf62ad22875f2
harsh <harshnew325@gmail.com>
2026-01-22 16:10:48 +0530

    fixing prisma.service.ts

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -3,17 +3,7 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-    constructor() {
-        const url = process.env.DATABASE_URL;
-        if (url?.startsWith('prisma://') || url?.startsWith('prisma+postgres://')) {
-            super({ accelerateUrl: url });
-        } else {
-            // Standard postgresql:// or postgres:// connections
-            super();
-        }
-    }
-
-    async onModuleInit() {
-        await this.$connect();
-    }
+  async onModuleInit() {
+    await this.$connect();
+  }
 }
